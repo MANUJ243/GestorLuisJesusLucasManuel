f7c07ec557f724236cbb6ebeb482f3cb03874a72
Lucas <lucas1sanz96@gmail.com>
2018-02-07 20:57:21 +0100

    Piechart to guapo e intento de captura(fallido)

--- src/view/VistaEstadisticasController.java
+++ src/view/VistaEstadisticasController.java
@@ -2,13 +2,28 @@ package view;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Producto;
 
-public class VistaEstadisticasController {
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class VistaEstadisticasController{
+    @FXML
+    private AnchorPane root = new AnchorPane();
+
     @FXML
     private PieChart grafico = new PieChart();
 
@@ -26,16 +41,28 @@ public class VistaEstadisticasController {
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
 
         //Meto los nombres y stock en sus respectivas listas observables
-        for (Object p:datosProducto
-             ) {
+        for (Object p:datosProducto) {
             pieChartData.add(new PieChart.Data(((Producto) p).getNombre(), Double.valueOf(((Producto) p).getStock())));
         }
 
+        //Meto los datos en el grafo y le quito los labels y la leyenda
         this.grafico.setData(pieChartData);
         this.grafico.setTitle("Stock");
-        grafico.setVisible(true);
+        this.grafico.setLegendVisible(false);
+        this.grafico.setAnimated(false);
+        this.grafico.setVisible(true);
 
+        //EventHandler para cada trozo de tarta
+        for (final PieChart.Data dt:grafico.getData()) {
+            dt.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED, e -> grafico.setTitle("Producto: " + dt.getName() + "; Stock: " + dt.getPieValue()));
+        }
 
+        //Intento de captura
+        WritableImage image = root.snapshot(null, null);
+        File file = new File("src/snaps/chart.png");
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        }catch (IOException ignored) {}
     }
 
     public void setEscenarioEstadisticas(Stage escenarioEstadisticas) {
